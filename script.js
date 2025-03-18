ed7299a33b44cdca6afc031586e9d531295841e5
fiweb <72560165+fiweb@users.noreply.github.com>
2025-03-18 10:08:39 +0100

    Update script.js
    Changed toFixed to 2 decimal places

--- script.js
+++ script.js
@@ -53,7 +53,7 @@ document.addEventListener("DOMContentLoaded", function () {
     if (Number.isInteger(number)) {
       return number.toString();
     } else {
-      return number.toFixed(1);
+      return number.toFixed(2);
     }
   }
 
